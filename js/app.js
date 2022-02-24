05fa041218958ad88ede2eef3988b33c07a8e58e
Leonel Montiel <70177138+leonelmontiel@users.noreply.github.com>
2022-02-23 21:20:30 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,30 +1,12 @@
-window.addEventListener('load', function() {
-    new Glider(document.querySelector('.carousel__lista'), {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        draggable: true,
-        dots: '.carousel__indicadores',
-        arrows: {
-            prev: '.carousel__anterior',
-            next: '.carousel__siguiente'
-        },
-        responsive: [
-            {
-              // screens greater than >= 800px
-              breakpoint: 800,
-              settings: {
-                // Set to `auto` and provide item width to adjust to viewport
-                slidesToShow: 2,
-                slidesToScroll: 2
-              }
-            },{
-              // screens greater than >= 1024px
-              breakpoint: 1024,
-              settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3
-              }
-            }
-          ]
-    });
+const btn_menu = document.querySelector(".btn__menu");
+const nav_menu = document.querySelector(".menu");
+
+btn_menu.addEventListener("click", () => {
+  nav_menu.classList.toggle("menu__visible");
+
+  if (nav_menu.classList.contains("menu__visible")) {
+    btn_menu.setAttribute("aria-label", "Cerrar menú");
+  } else {
+    btn_menu.setAttribute("aria-label", "Abrir menú");
+  }
 });
